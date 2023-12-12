69b94bb49041d46733bc14b5d657dbf1e0d6bb04
walidbenf <45196746+walidbenf@users.noreply.github.com>
2023-12-12 17:28:38 +0100

    Add files via upload

---
+++ front/frontend/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<HomePage />
+	</div>
+  </template>
+  
+<script>
+import HomePage from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color:#474444;
+  height: 100vh;
+}
+</style>
